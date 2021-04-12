4bde92a94fe2c14a5b0165faa24486812a3b5ac9
aar-atiq <aar.msdeveloper01@gmail.com>
2021-04-13 01:53:50 +0600

    created post request

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config() 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.skqkk.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
 const app = express()
 const port = 5000
 
@@ -15,6 +17,20 @@ app.get('/', (req, res) => {
 })
 
 
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const appointmentCollection = client.db("doctorsPortal").collection("appointments");
+  
+  app.post("/addAppointment", (req, res) => {
+      const appointment = req.body;
+      appointmentCollection.insertOne(appointment)
+      .then(result => {
+          res.send(result.insertedCount);
+      })
+  })
+
+});
 
 
 app.listen(process.env.PORT || port)
